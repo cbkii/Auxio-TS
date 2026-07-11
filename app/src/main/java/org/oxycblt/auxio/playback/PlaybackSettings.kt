4847949a0db8e7d7db26a72624533f36179a0303
cbkii <150587541+cbkii@users.noreply.github.com>
2026-07-12 00:59:04 +1000

    fix: align startup panel preference defaults

--- app/src/main/java/org/oxycblt/auxio/playback/PlaybackSettings.kt
+++ app/src/main/java/org/oxycblt/auxio/playback/PlaybackSettings.kt
@@ -22,7 +22,6 @@ import android.content.Context
 import androidx.core.content.edit
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
-import org.oxycblt.auxio.BuildConfig
 import org.oxycblt.auxio.IntegerTable
 import org.oxycblt.auxio.R
 import org.oxycblt.auxio.playback.replaygain.ReplayGainMode
@@ -86,6 +85,8 @@ interface PlaybackSettings : Settings<PlaybackSettings.Listener> {
 
 class PlaybackSettingsImpl @Inject constructor(@ApplicationContext context: Context) :
     Settings.Impl<PlaybackSettings.Listener>(context), PlaybackSettings {
+    private val launchToPanelDefault = context.resources.getBoolean(R.bool.launch_to_panel_default)
+
     override val playInListWith: PlaySong
         get() =
             PlaySong.fromIntCode(
@@ -159,7 +160,7 @@ class PlaybackSettingsImpl @Inject constructor(@ApplicationContext context: Cont
         get() =
             sharedPreferences.getBoolean(
                 getString(R.string.set_key_launch_to_panel),
-                BuildConfig.TOPWAY_COMPAT_FLAVOR,
+                launchToPanelDefault,
             )
 
     override val autoplayOnLaunch: Boolean
