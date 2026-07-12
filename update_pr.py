53cd45f230068567b9dec6dc2390a2eaf56b1f8c
cbkii <150587541+cbkii@users.noreply.github.com>
2026-07-12 13:09:36 +1000

    chore: remove temporary PR helper

--- update_pr.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import os
-
-def check_branch():
-    result = subprocess.run(["git", "branch", "--show-current"], capture_output=True, text=True)
-    return result.stdout.strip()
-
-print("Current branch:", check_branch())
