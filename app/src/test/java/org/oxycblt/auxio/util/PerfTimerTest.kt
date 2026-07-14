0b6566dc1dc973ff12a553d95c05d44d8f001d7a
cbkii <150587541+cbkii@users.noreply.github.com>
2026-07-15 00:01:09 +1000

    test: run performance timer checks with API 29 Robolectric

--- app/src/test/java/org/oxycblt/auxio/util/PerfTimerTest.kt
+++ app/src/test/java/org/oxycblt/auxio/util/PerfTimerTest.kt
@@ -22,7 +22,12 @@ import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [29])
 class PerfTimerTest {
     @After
     fun tearDown() {
