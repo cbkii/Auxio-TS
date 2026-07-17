13bf21846a8da153e8ecc2c29cb74712a05ebe91
cbkii <150587541+cbkii@users.noreply.github.com>
2026-07-18 03:11:07 +1000

    feat: centralise incremental source planning

---
+++ app/src/main/java/org/oxycblt/auxio/music/IncrementalIndexPlanner.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2026 Auxio Project
+ * IncrementalIndexPlanner.kt is part of Auxio.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+package org.oxycblt.auxio.music
+
+import org.oxycblt.musikr.cache.IncrementalCache
+import org.oxycblt.musikr.cache.IncrementalScanPlan
+import org.oxycblt.musikr.cache.MutableCache
+import org.oxycblt.musikr.fs.FS
+import org.oxycblt.musikr.fs.SourceAwareFS
+import org.oxycblt.musikr.library.MetadataProfile
+
+internal object IncrementalIndexPlanner {
+    data class Prepared(
+        val fs: FS,
+        val cache: MutableCache,
+        val plan: IncrementalScanPlan?,
+    )
+
+    suspend fun prepare(
+        fs: FS,
+        cache: MutableCache,
+        withCache: Boolean,
+        profile: MetadataProfile,
+        configurationRevision: Long,
+        legacyWriteOnly: (MutableCache) -> MutableCache,
+    ): Prepared {
+        val incremental = cache as? IncrementalCache
+        val sourceAware = fs as? SourceAwareFS
+        if (incremental == null || sourceAware == null) {
+            return Prepared(
+                fs = fs,
+                cache = if (withCache) cache else legacyWriteOnly(cache),
+                plan = null,
+            )
+        }
+
+        val plan =
+            incremental.planScan(
+                snapshots = sourceAware.sourceSnapshots(),
+                force = !withCache,
+                metadataProfile = profile,
+                configurationRevision = configurationRevision,
+            )
+        return Prepared(
+            fs = sourceAware.selectSources(plan.scanSourceKeys),
+            cache = cache,
+            plan = plan,
+        )
+    }
+}
