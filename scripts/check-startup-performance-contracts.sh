72228b433fc0d9fd111e480f4b38dcf9793de5a3
cbkii <150587541+cbkii@users.noreply.github.com>
2026-07-19 03:02:54 +1000

    fix: validate app-local baseline profile configuration

--- scripts/check-startup-performance-contracts.sh
+++ scripts/check-startup-performance-contracts.sh
@@ -60,8 +60,9 @@ done
 
 require_contains settings.gradle "include ':startup-benchmark'"
 require_contains build.gradle 'id "androidx.baselineprofile" version "1.4.1" apply false'
-require_contains build.gradle 'minifyEnabled false'
-require_contains build.gradle 'dependencies.add("baselineProfile", project(":startup-benchmark"))'
+require_contains app/build.gradle 'id "androidx.baselineprofile"'
+require_contains app/build.gradle 'minifyEnabled false'
+require_contains app/build.gradle 'baselineProfile project(":startup-benchmark")'
 require_contains app/build.gradle 'implementation "androidx.profileinstaller:profileinstaller:1.4.1"'
 require_contains startup-benchmark/build.gradle 'id "com.android.test"'
 require_contains startup-benchmark/build.gradle 'id "androidx.baselineprofile"'
