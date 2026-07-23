7f0e60cf954a0a6b2d31b18d679ac2882fd33520
cbkii <150587541+cbkii@users.noreply.github.com>
2026-07-24 09:21:10 +1000

    feat(media): define canonical session initialization policy

---
+++ app/src/main/java/org/oxycblt/auxio/playback/service/MediaSessionInitializationPolicy.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2026 Auxio Project
+ * MediaSessionInitializationPolicy.kt is part of Auxio.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+package org.oxycblt.auxio.playback.service
+
+import android.support.v4.media.session.MediaSessionCompat
+import android.support.v4.media.session.PlaybackStateCompat
+
+/** Pure values installed before the canonical playback session becomes active. */
+object MediaSessionInitializationPolicy {
+    const val FLAGS =
+        MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS or
+            MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS or
+            MediaSessionCompat.FLAG_HANDLES_QUEUE_COMMANDS
+
+    fun initialPlaybackState(): PlaybackStateCompat =
+        PlaybackStateCompat.Builder()
+            .setActions(MediaSessionInterface.ACTIONS)
+            .setState(PlaybackStateCompat.STATE_NONE, 0L, 0f)
+            .build()
+}
