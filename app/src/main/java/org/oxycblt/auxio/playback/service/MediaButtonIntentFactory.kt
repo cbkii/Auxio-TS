49be76c9690545ab8b89ce21b3911dc1e0042d40
cbkii <150587541+cbkii@users.noreply.github.com>
2026-07-24 08:20:10 +1000

    feat(media): centralize canonical media-button intents

---
+++ app/src/main/java/org/oxycblt/auxio/playback/service/MediaButtonIntentFactory.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2026 Auxio Project
+ * MediaButtonIntentFactory.kt is part of Auxio.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+package org.oxycblt.auxio.playback.service
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.view.KeyEvent
+import org.oxycblt.auxio.AuxioService
+import org.oxycblt.auxio.IntegerTable
+
+/** Builds explicit media-button intents for Auxio's one canonical playback service/session. */
+object MediaButtonIntentFactory {
+    fun receiverComponent(context: Context): ComponentName =
+        ComponentName(context, MediaButtonReceiver::class.java)
+
+    fun receiverIntent(context: Context, keyCode: Int): Intent =
+        Intent(Intent.ACTION_MEDIA_BUTTON)
+            .setComponent(receiverComponent(context))
+            .putExtra(Intent.EXTRA_KEY_EVENT, KeyEvent(KeyEvent.ACTION_DOWN, keyCode))
+
+    fun serviceIntent(
+        context: Context,
+        serviceClass: Class<*>,
+        keyCode: Int,
+    ): Intent =
+        Intent(context, serviceClass)
+            .setAction(Intent.ACTION_MEDIA_BUTTON)
+            .putExtra(Intent.EXTRA_KEY_EVENT, KeyEvent(KeyEvent.ACTION_DOWN, keyCode))
+            .putExtra(AuxioService.INTENT_KEY_START_ID, IntegerTable.START_ID_MEDIA_BUTTON)
+}
