30dff79c48991dc49aca58f2aeb4ba3cdedfaff6
cbkii <150587541+cbkii@users.noreply.github.com>
2026-07-24 20:27:05 +1000

    fix(ts18): configure Magisk service entrypoint

--- tools/ts18-root-storage-fastpath/magisk-module/customize.sh
+++ tools/ts18-root-storage-fastpath/magisk-module/customize.sh
@@ -1,5 +1,6 @@
 #!/system/bin/sh
 ui_print "- Installing Auxio-TS TS18 root storage helper"
+ui_print "- Uses Magisk module-root service.sh for bounded late-start preparation"
 ui_print "- No /system write, package disable, app launch or library scan is performed"
 set_perm_recursive "$MODPATH" 0 0 0755 0644
-set_perm "$MODPATH/service.d/55-auxio-root-storage-prepare.sh" 0 0 0755
+set_perm "$MODPATH/service.sh" 0 0 0755
