6b66e85ec0bec231541f46726ef7dd5d3df462a7
cbkii <150587541+cbkii@users.noreply.github.com>
2026-07-25 18:08:03 +1000

    style: apply Kotlin formatting to commit-policy tests

--- musikr/src/test/java/org/oxycblt/musikr/SourceScanCommitPolicyTest.kt
+++ musikr/src/test/java/org/oxycblt/musikr/SourceScanCommitPolicyTest.kt
@@ -6,6 +6,14 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package org.oxycblt.musikr
@@ -29,9 +37,7 @@ class SourceScanCommitPolicyTest {
     @Test
     fun successfulEmptySourceMayPublishConfirmedEmptyLibrary() {
         assertFalse(
-            SourceScanCommitPolicy.rejectsAsAuthoritativeEmpty(
-                commit(committed = setOf("source"))
-            )
+            SourceScanCommitPolicy.rejectsAsAuthoritativeEmpty(commit(committed = setOf("source")))
         )
     }
 
@@ -39,10 +45,7 @@ class SourceScanCommitPolicyTest {
     fun mixedFailureAndReusedGenerationRemainsReadable() {
         assertFalse(
             SourceScanCommitPolicy.rejectsAsAuthoritativeEmpty(
-                commit(
-                    reused = setOf("internal"),
-                    failed = mapOf("usb" to "unmounted"),
-                )
+                commit(reused = setOf("internal"), failed = mapOf("usb" to "unmounted"))
             )
         )
     }
