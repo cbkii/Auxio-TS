f68d15e23db4b5a1e534c5cd8421ca0d01b9c9af
cbkii <150587541+cbkii@users.noreply.github.com>
2026-08-11 02:58:19 +1000

    Test artwork repair trigger policy

---
+++ app/src/test/java/org/oxycblt/auxio/music/ArtworkRepairPolicyTest.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2026 Auxio Project
+ * ArtworkRepairPolicyTest.kt is part of Auxio.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.oxycblt.auxio.music
+
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import org.oxycblt.auxio.image.CoverMode
+
+class ArtworkRepairPolicyTest {
+    @Test
+    fun `enabled artwork waits until full library is ready`() {
+        assertFalse(
+            ArtworkRepairPolicy.shouldRequest(
+                CoverMode.OPTIMISED,
+                StartupReadinessState.QueueReady,
+            )
+        )
+        assertTrue(
+            ArtworkRepairPolicy.shouldRequest(
+                CoverMode.OPTIMISED,
+                StartupReadinessState.FullLibraryReady,
+            )
+        )
+    }
+
+    @Test
+    fun `off never schedules artwork repair`() {
+        assertFalse(
+            ArtworkRepairPolicy.shouldRequest(
+                CoverMode.OFF,
+                StartupReadinessState.EnrichmentComplete,
+            )
+        )
+    }
+
+    @Test
+    fun `as is mode also schedules repair`() {
+        assertTrue(
+            ArtworkRepairPolicy.shouldRequest(
+                CoverMode.AS_IS,
+                StartupReadinessState.FullLibraryReady,
+            )
+        )
+    }
+}
