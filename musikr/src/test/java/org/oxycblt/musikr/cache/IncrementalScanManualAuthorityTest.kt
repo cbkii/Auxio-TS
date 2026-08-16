cf30724f16ae123cb6e315e147115e39d272459f
cbkii <150587541+cbkii@users.noreply.github.com>
2026-08-16 12:58:22 +1000

    style: normalise incremental scan authority test

--- musikr/src/test/java/org/oxycblt/musikr/cache/IncrementalScanManualAuthorityTest.kt
+++ musikr/src/test/java/org/oxycblt/musikr/cache/IncrementalScanManualAuthorityTest.kt
@@ -6,6 +6,14 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package org.oxycblt.musikr.cache
@@ -106,8 +114,7 @@ class IncrementalScanManualAuthorityTest {
             metadataProfile = profile,
             configurationRevision = 1L,
             force = false,
-            scanReasons =
-                mapOf(source.sourceKey to SourceScanReason.ADVISORY_FINGERPRINT_EXPIRED),
+            scanReasons = mapOf(source.sourceKey to SourceScanReason.ADVISORY_FINGERPRINT_EXPIRED),
         )
 
     private class TestCache(private val plan: IncrementalScanPlan) : IncrementalCache {
