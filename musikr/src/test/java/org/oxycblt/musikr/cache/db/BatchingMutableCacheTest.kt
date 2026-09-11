ede71c5c6d5329ac5e039b0141ccdf4cf8af301c
cbkii <150587541+cbkii@users.noreply.github.com>
2026-09-12 02:37:24 +1000

    style: add batching cache test source header

--- musikr/src/test/java/org/oxycblt/musikr/cache/db/BatchingMutableCacheTest.kt
+++ musikr/src/test/java/org/oxycblt/musikr/cache/db/BatchingMutableCacheTest.kt
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2026 Auxio Project
+ * BatchingMutableCacheTest.kt is part of Auxio.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package org.oxycblt.musikr.cache.db
 
 import android.content.Context
@@ -33,7 +51,10 @@ class BatchingMutableCacheTest {
     @Before
     fun setUp() {
         val context = ApplicationProvider.getApplicationContext<Context>()
-        db = Room.inMemoryDatabaseBuilder(context, CacheDatabase::class.java).allowMainThreadQueries().build()
+        db =
+            Room.inMemoryDatabaseBuilder(context, CacheDatabase::class.java)
+                .allowMainThreadQueries()
+                .build()
         cache = BatchingMutableCache.from(db)
     }
 
@@ -66,7 +87,10 @@ class BatchingMutableCacheTest {
         )
 
         cache.commitScan()
-        assertEquals(BatchingMutableCache.WRITE_BATCH_SIZE + 1, db.incrementalLibraryDao().songCount())
+        assertEquals(
+            BatchingMutableCache.WRITE_BATCH_SIZE + 1,
+            db.incrementalLibraryDao().songCount(),
+        )
     }
 
     @Test
