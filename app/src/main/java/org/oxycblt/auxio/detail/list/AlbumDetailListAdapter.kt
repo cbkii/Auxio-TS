3025e3f51997d0a6177436604c1ae20e888347f8
cbkii <150587541+cbkii@users.noreply.github.com>
2026-09-12 02:38:41 +1000

    recycler: give disc dividers explicit layout params

--- app/src/main/java/org/oxycblt/auxio/detail/list/AlbumDetailListAdapter.kt
+++ app/src/main/java/org/oxycblt/auxio/detail/list/AlbumDetailListAdapter.kt
@@ -181,7 +181,16 @@ class DiscDividerViewHolder private constructor(divider: MaterialDivider) :
          * @param parent The parent to inflate this instance from.
          * @return A new instance.
          */
-        fun from(parent: ViewGroup) = DiscDividerViewHolder(MaterialDivider(parent.context))
+        fun from(parent: ViewGroup) =
+            DiscDividerViewHolder(
+                MaterialDivider(parent.context).apply {
+                    layoutParams =
+                        RecyclerView.LayoutParams(
+                            RecyclerView.LayoutParams.MATCH_PARENT,
+                            RecyclerView.LayoutParams.WRAP_CONTENT,
+                        )
+                }
+            )
 
         /** A comparator that can be used with DiffUtil. */
         val DIFF_CALLBACK =
@@ -219,8 +228,7 @@ private class AlbumSongViewHolder private constructor(private val binding: ItemA
             }
             binding.songTrackPlaceholder.isInvisible = true
         } else {
-            binding.songTrackCover.contentDescription =
-                binding.context.getString(R.string.def_track)
+            binding.songTrackCover.contentDescription = binding.context.getString(R.string.def_track)
             binding.songTrackText.apply {
                 isInvisible = true
                 text = null
