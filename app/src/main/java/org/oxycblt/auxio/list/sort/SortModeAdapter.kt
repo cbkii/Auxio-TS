79d6e29d88a07e7e879f8f258b530698fba4e4c7
cbkii <150587541+cbkii@users.noreply.github.com>
2026-09-11 17:20:10 +1000

    fix(ui): use normal RecyclerView holder for sort choices

--- app/src/main/java/org/oxycblt/auxio/list/sort/SortModeAdapter.kt
+++ app/src/main/java/org/oxycblt/auxio/list/sort/SortModeAdapter.kt
@@ -20,10 +20,10 @@ package org.oxycblt.auxio.list.sort
 
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
 import org.oxycblt.auxio.databinding.ItemSortModeBinding
 import org.oxycblt.auxio.list.ClickableListListener
 import org.oxycblt.auxio.list.adapter.FlexibleListAdapter
-import org.oxycblt.auxio.list.recycler.DialogRecyclerView
 import org.oxycblt.auxio.util.context
 import org.oxycblt.auxio.util.inflater
 
@@ -77,12 +77,12 @@ class SortModeAdapter(private val listener: ClickableListListener<Sort.Mode>) :
 }
 
 /**
- * A [DialogRecyclerView.ViewHolder] that displays a [Sort.Mode].
+ * A [RecyclerView.ViewHolder] that displays a [Sort.Mode].
  *
  * @author Alexander Capehart (OxygenCobalt)
  */
 class SortModeViewHolder private constructor(private val binding: ItemSortModeBinding) :
-    DialogRecyclerView.ViewHolder(binding.root) {
+    RecyclerView.ViewHolder(binding.root) {
     /**
      * Bind new data to this instance.
      *
