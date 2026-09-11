e29fe6d23707c4dabae568b17702757f335989ad
cbkii <150587541+cbkii@users.noreply.github.com>
2026-09-12 02:32:29 +1000

    recycler: give detail dividers explicit layout params

--- app/src/main/java/org/oxycblt/auxio/detail/list/DetailListAdapter.kt
+++ app/src/main/java/org/oxycblt/auxio/detail/list/DetailListAdapter.kt
@@ -6,14 +6,6 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package org.oxycblt.auxio.detail.list
@@ -38,14 +30,6 @@ import org.oxycblt.auxio.util.context
 import org.oxycblt.auxio.util.inflater
 import org.oxycblt.musikr.Music
 
-/**
- * A [RecyclerView.Adapter] that implements shared behavior between lists of child items in the
- * detail views.
- *
- * @param listener A [Listener] to bind interactions to.
- * @param diffCallback A [DiffUtil.ItemCallback] to compare list updates with.
- * @author Alexander Capehart (OxygenCobalt)
- */
 abstract class DetailListAdapter(
     private val listener: Listener<*>,
     private val diffCallback: DiffUtil.ItemCallback<Item>,
@@ -53,7 +37,6 @@ abstract class DetailListAdapter(
 
     override fun getItemViewType(position: Int) =
         when (getItem(position)) {
-            // Implement support for headers and sort headers
             is PlainDivider -> DividerViewHolder.VIEW_TYPE
             is BasicHeader -> BasicHeaderViewHolder.VIEW_TYPE
             is SortHeader -> SortHeaderViewHolder.VIEW_TYPE
@@ -62,7 +45,14 @@ abstract class DetailListAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         when (viewType) {
-            DividerViewHolder.VIEW_TYPE -> DividerViewHolder.from(parent)
+            DividerViewHolder.VIEW_TYPE ->
+                DividerViewHolder.from(parent).apply {
+                    itemView.layoutParams =
+                        RecyclerView.LayoutParams(
+                            RecyclerView.LayoutParams.MATCH_PARENT,
+                            RecyclerView.LayoutParams.WRAP_CONTENT,
+                        )
+                }
             BasicHeaderViewHolder.VIEW_TYPE -> BasicHeaderViewHolder.from(parent)
             SortHeaderViewHolder.VIEW_TYPE -> SortHeaderViewHolder.from(parent)
             else -> error("Invalid item type $viewType")
@@ -75,17 +65,11 @@ abstract class DetailListAdapter(
         }
     }
 
-    /** An extended [SelectableListListener] for [DetailListAdapter] implementations. */
     interface Listener<in T : Music> : SelectableListListener<T> {
-        /**
-         * Called when the button in a [SortHeader] item is pressed, requesting that the sort menu
-         * should be opened.
-         */
         fun onOpenSortMenu()
     }
 
     protected companion object {
-        /** A comparator that can be used with DiffUtil. */
         val DIFF_CALLBACK =
             object : SimpleDiffCallback<Item>() {
                 override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean {
@@ -103,54 +87,26 @@ abstract class DetailListAdapter(
     }
 }
 
-/**
- * A header variation that displays a button to open a sort menu.
- *
- * @param titleRes The string resource to use as the header title
- * @author Alexander Capehart (OxygenCobalt)
- */
 data class SortHeader(@StringRes override val titleRes: Int) : PlainHeader
 
-/**
- * A [RecyclerView.ViewHolder] that displays a [SortHeader] and it's actions. Use [from] to create
- * an instance.
- *
- * @author Alexander Capehart (OxygenCobalt)
- */
 private class SortHeaderViewHolder(private val binding: ItemSortHeaderBinding) :
     RecyclerView.ViewHolder(binding.root) {
-    /**
-     * Bind new data to this instance.
-     *
-     * @param sortHeader The new [SortHeader] to bind.
-     * @param listener An [DetailListAdapter.Listener] to bind interactions to.
-     */
     fun bind(sortHeader: SortHeader, listener: DetailListAdapter.Listener<*>) {
         binding.headerTitle.text = binding.context.getString(sortHeader.titleRes)
         binding.headerSort.apply {
-            // Add a Tooltip based on the content description so that the purpose of this
-            // button can be clear.
             TooltipCompat.setTooltipText(this, contentDescription)
             setOnClickListener { listener.onOpenSortMenu() }
         }
     }
 
     companion object {
-        /** A unique ID for this [RecyclerView.ViewHolder] type. */
         const val VIEW_TYPE = IntegerTable.VIEW_TYPE_SORT_HEADER
 
-        /**
-         * Create a new instance.
-         *
-         * @param parent The parent to inflate this instance from.
-         * @return A new instance.
-         */
         fun from(parent: ViewGroup) =
             SortHeaderViewHolder(
                 ItemSortHeaderBinding.inflate(parent.context.inflater, parent, false)
             )
 
-        /** A comparator that can be used with DiffUtil. */
         val DIFF_CALLBACK =
             object : SimpleDiffCallback<SortHeader>() {
                 override fun areContentsTheSame(oldItem: SortHeader, newItem: SortHeader) =
